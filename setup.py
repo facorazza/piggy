7500e3c20ea560094d1818263808cb5d95a39ef7
SuperHelios <20555025+Imperator26@users.noreply.github.com>
2019-06-21 09:11:17 +0200

    Fix install requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="piggy",
-    version="0.0.1",
+    version="0.0.2",
     author="Federico A. Corazza",
     author_email="federico.corazza@live.it",
     description="Piggy is an asynchronous Python library which helps managing Instagram accounts and facilitates their growth.",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Imperator26/piggy",
     packages=setuptools.find_packages(),
+    install_requires=['requests', 'asyncio', 'aiohttp', 'aiosqlite', 'aiofiles', 'regex'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
